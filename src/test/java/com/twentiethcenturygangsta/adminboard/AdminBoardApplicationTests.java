ab2a0e702af34e63ebdaa868b8047111da7962f8
oereo <dlstpgns0406@gmail.com>
2023-05-07 22:29:48 +0900

    chore: project setting

---
+++ src/test/java/com/twentiethcenturygangsta/adminboard/AdminBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.twentiethcenturygangsta.adminboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdminBoardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
