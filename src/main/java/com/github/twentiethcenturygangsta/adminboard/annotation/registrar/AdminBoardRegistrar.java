633b13ab6c9ca11d359cde86adaf2a3bd2798d7d
oereo <dlstpgns0406@gmail.com>
2023-05-10 18:58:26 +0900

    feat: AdminBoardRegistrar class 추가
    - EnableAdminBoard annotation의 basePackages를 등록할 경우 String[] type으로 basePackage name을 등록
    - EnableAdminBoard annotation의 basePackages가 없을 경우 EnableAdminBoard annotation이 붙은 class의 packageName을 등록

---
+++ src/main/java/com/github/twentiethcenturygangsta/adminboard/annotation/registrar/AdminBoardRegistrar.java
@@ -0,0 +1,52 @@
+package com.github.twentiethcenturygangsta.adminboard.annotation.registrar;
+
+import com.github.twentiethcenturygangsta.adminboard.annotation.EnableAdminBoard;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.support.BeanDefinitionRegistry;
+import org.springframework.boot.autoconfigure.AutoConfigurationImportSelector;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.ImportBeanDefinitionRegistrar;
+import org.springframework.context.annotation.ImportSelector;
+import org.springframework.core.annotation.AnnotationAttributes;
+import org.springframework.core.type.AnnotationMetadata;
+import org.springframework.util.Assert;
+import org.springframework.util.ClassUtils;
+
+import java.util.*;
+
+@Slf4j
+public class AdminBoardRegistrar implements ImportBeanDefinitionRegistrar {
+
+    private String[] basePackages;
+
+
+    @Override
+    public void registerBeanDefinitions(AnnotationMetadata importingClassMetadata, BeanDefinitionRegistry registry) {
+        AnnotationAttributes attributes = getAttributes(importingClassMetadata);
+        String[] basePackages = attributes.getStringArray("basePackages");
+
+        if (basePackages.length == 0) {
+            this.basePackages = new String[]{ClassUtils.getPackageName(importingClassMetadata.getClassName())};
+        } else {
+            this.basePackages = basePackages;
+        }
+    }
+
+    public String[] getBasePackages() {
+        return basePackages;
+    }
+
+    protected Class<?> getAnnotationClass() {
+        return EnableAdminBoard.class;
+    }
+
+    protected AnnotationAttributes getAttributes(AnnotationMetadata metadata) {
+        String name = getAnnotationClass().getName();
+        AnnotationAttributes attributes = AnnotationAttributes.fromMap(metadata.getAnnotationAttributes(name, false));
+        Assert.notNull(attributes, () -> "No auto-configuration attributes found. Is " + metadata.getClassName()
+                + " annotated with " + ClassUtils.getShortName(name) + "?");
+        return attributes;
+    }
+}
