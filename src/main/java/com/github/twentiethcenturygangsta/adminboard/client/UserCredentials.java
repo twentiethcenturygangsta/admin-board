21412fa3e94bf6614847d6343c58133bf81fe697
oereo <dlstpgns0406@gmail.com>
2023-05-11 19:39:03 +0900

    feat: UserCredentials class 추가
    - Super admin 계정에 대한 아이디와 비밀번호를 관리하는 class

---
+++ src/main/java/com/github/twentiethcenturygangsta/adminboard/client/UserCredentials.java
@@ -0,0 +1,42 @@
+package com.github.twentiethcenturygangsta.adminboard.client;
+
+import com.github.twentiethcenturygangsta.adminboard.enums.Role;
+import lombok.Builder;
+
+public class UserCredentials {
+    private final String memberId;
+    private final String password;
+    private final Role role;
+
+    /**
+     * Constructs a new UserDatabaseCredentials object, with the specified user database
+     * endpoint, id and password.
+     *
+     * @param memberId
+     *            The user login id.
+     * @param password
+     *            The user login password.
+     *
+     * @exception IllegalArgumentException when there is no value among encryptKey, memberId, password
+     */
+    @Builder
+    public UserCredentials(String memberId, String password) {
+        if (memberId == null) {
+            throw new IllegalArgumentException("MemberId cannot be null.");
+        }
+        if (password == null) {
+            throw new IllegalArgumentException("Password cannot be null");
+        }
+        this.memberId = memberId;
+        this.password = password;
+        this.role = Role.SUPER_ADMIN;
+    }
+
+    public String getMemberId() {
+        return memberId;
+    }
+
+    public Role getRole() {
+        return role;
+    }
+}
