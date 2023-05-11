beceae0a309740377a5fc901c82ac5cc7f6b7847
oereo <dlstpgns0406@gmail.com>
2023-05-11 19:40:31 +0900

    feat: AdminBoardClient class 추가
    - UserCredentials 객체와 AdminBoardInfo 객체 관리

---
+++ src/main/java/com/github/twentiethcenturygangsta/adminboard/client/AdminBoardClient.java
@@ -0,0 +1,23 @@
+package com.github.twentiethcenturygangsta.adminboard.client;
+
+import lombok.Builder;
+
+public class AdminBoardClient {
+
+    private final UserCredentials userCredentials;
+    private final AdminBoardInfo adminBoardInfo;
+
+    @Builder
+    public AdminBoardClient(UserCredentials userCredentials, AdminBoardInfo adminBoardInfo) {
+        this.userCredentials = userCredentials;
+        this.adminBoardInfo = adminBoardInfo;
+    }
+
+    public UserCredentials getUserCredentials() {
+        return userCredentials;
+    }
+
+    public AdminBoardInfo getAdminBoardInfo() {
+        return adminBoardInfo;
+    }
+}
