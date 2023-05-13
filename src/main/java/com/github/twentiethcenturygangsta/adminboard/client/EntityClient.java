233c82602cd173c569ad41edae5b3622dce42aa6
oereo <dlstpgns0406@gmail.com>
2023-05-13 23:52:24 +0900

    feat: EntityClient entityClasses에 entity class list 저장

--- src/main/java/com/github/twentiethcenturygangsta/adminboard/client/EntityClient.java
+++ src/main/java/com/github/twentiethcenturygangsta/adminboard/client/EntityClient.java
@@ -1,10 +1,13 @@
 package com.github.twentiethcenturygangsta.adminboard.client;
 
+import com.github.twentiethcenturygangsta.adminboard.AdminBoardRepositoryBuilder;
 import com.github.twentiethcenturygangsta.adminboard.annotation.AdminBoardEntity;
 import com.github.twentiethcenturygangsta.adminboard.annotation.registrar.AdminBoardRegistrar;
 import com.github.twentiethcenturygangsta.adminboard.entity.EntityInfo;
 import lombok.extern.slf4j.Slf4j;
 import org.reflections.Reflections;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
 
 import java.util.*;
 
@@ -12,6 +15,7 @@ import java.util.*;
 public class EntityClient {
 
     private List<EntityInfo> entities;
+    private Set<Class<?>> entityClasses;
 
     public void registerEntities() {
         List<EntityInfo> entities = new ArrayList<>();
@@ -25,11 +29,28 @@ public class EntityClient {
         for (Class<?> entity : entityClasses) {
             entities.add(EntityInfo.builder().object(entity).build());
         }
-
+        this.entityClasses = entityClasses;
         this.entities = entities;
     }
 
     public List<EntityInfo> getEntities() {
         return entities;
+    }
+    public Set<Class<?>> getEntityClasses() {
+        return entityClasses;
+    }
+
+    public EntityInfo getEntity(String entityName) {
+        return entities.stream()
+                .filter(o -> o.getName().equals(entityName))
+                .findAny()
+                .orElseThrow(() -> new RuntimeException("Not exist matched entity"));
+    }
+
+    public Class<?> getEntityClass(String entityName) {
+        return entityClasses.stream()
+                .filter(o -> o.getSimpleName().equals(entityName))
+                .findAny()
+                .orElseThrow(() -> new RuntimeException("Not exist matched entityClass"));
     }
 }
