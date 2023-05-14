eabbadced75ffb028a5b249d64d41f5be3d686b3
oereo <dlstpgns0406@gmail.com>
2023-05-14 21:40:15 +0900

    feat: DefaultListableBeanFactory bean과 AdminBoardRepositoryBuilder Bean 등록

--- src/main/java/com/github/twentiethcenturygangsta/adminboard/AdminBoardAutoConfiguration.java
+++ src/main/java/com/github/twentiethcenturygangsta/adminboard/AdminBoardAutoConfiguration.java
@@ -1,10 +1,11 @@
 package com.github.twentiethcenturygangsta.adminboard;
 
 import com.github.twentiethcenturygangsta.adminboard.client.EntityClient;
+import org.springframework.beans.factory.support.DefaultListableBeanFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-@Configuration(proxyBeanMethods = false)
+@Configuration
 public class AdminBoardAutoConfiguration {
 
     @Bean
@@ -12,5 +13,18 @@ public class AdminBoardAutoConfiguration {
         EntityClient entityClient = new EntityClient();
         entityClient.registerEntities();
         return entityClient;
+    }
+
+    @Bean
+    public DefaultListableBeanFactory defaultListableBeanFactory() {
+        return new DefaultListableBeanFactory();
+    }
+
+    @Bean
+    public AdminBoardRepositoryBuilder adminBoardRepositoryBuilder() {
+        AdminBoardRepositoryBuilder adminBoardRepositoryBuilder = new AdminBoardRepositoryBuilder(defaultListableBeanFactory());
+        adminBoardRepositoryBuilder.registerRepositories();
+
+        return adminBoardRepositoryBuilder;
     }
 }
