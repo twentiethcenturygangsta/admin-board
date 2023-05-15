883d040f0263c520ab98d6375cf7f2c43aa17df7
oereo <dlstpgns0406@gmail.com>
2023-05-15 20:51:11 +0900

    feat: AdminBoardRepositoryBuilder class 추가
    - ApplicationContext 로부터 repositories 객체 가져오기
    - RepositoryInfo dto type으로 객체 관리

---
+++ src/main/java/com/github/twentiethcenturygangsta/adminboard/AdminBoardRepositoryBuilder.java
@@ -0,0 +1,60 @@
+package com.github.twentiethcenturygangsta.adminboard;
+
+import com.github.twentiethcenturygangsta.adminboard.annotation.registrar.AdminBoardRegistrar;
+import com.github.twentiethcenturygangsta.adminboard.repository.RepositoryInfo;
+import lombok.extern.slf4j.Slf4j;
+import org.reflections.Reflections;
+import org.springframework.context.ApplicationContext;
+
+import org.springframework.data.repository.core.support.DefaultRepositoryMetadata;
+import org.springframework.data.repository.support.Repositories;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Repository;
+
+import java.util.*;
+
+@Slf4j
+@Component
+public class AdminBoardRepositoryBuilder {
+    private final ApplicationContext applicationContext;
+    private final Map<String, RepositoryInfo> repositories = new HashMap<>();
+
+    public AdminBoardRepositoryBuilder(final ApplicationContext applicationContext) {
+        this.applicationContext = applicationContext;
+    }
+
+// domain : metadata.getDomainType()
+// repository id type : metadata.getIdType()
+// repository : metadata.getRepositoryInterface()
+// repository interfaces : metadata.getRepositoryInterface().getInterfaces()
+    public void registerRepositories() {
+
+        List<Class<?>> repositories = new ArrayList<>();
+        List<String> basePackages = AdminBoardRegistrar.getBasePackages();
+
+        for(String basePackage : basePackages) {
+            repositories.addAll(new Reflections(basePackage).getTypesAnnotatedWith(Repository.class));
+        }
+        for (Class<?> object : repositories) {
+            RepositoryInfo repositoryInfo = getRepository(object);
+            this.repositories.put(object.getSimpleName(), repositoryInfo);
+        }
+    }
+
+    public Map<String, RepositoryInfo> getRepositories() {
+        return repositories;
+    }
+
+    private RepositoryInfo getRepository(Class<?> object) {
+        Repositories repositoriesInApplicationContext = new Repositories(applicationContext);
+        DefaultRepositoryMetadata metadata = new DefaultRepositoryMetadata(object);
+        Object repository = repositoriesInApplicationContext.getRepositoryFor(metadata.getDomainType()).orElseThrow(() -> new RuntimeException("Not exist Repository"));
+
+        List<Class<?>> repositoryInterfaces = Arrays.asList(metadata.getRepositoryInterface().getInterfaces());
+        return RepositoryInfo.builder()
+                    .repositoryInterfaces(repositoryInterfaces)
+                    .repositoryObject(repository)
+                    .metaData(metadata)
+                    .build();
+    }
+}
