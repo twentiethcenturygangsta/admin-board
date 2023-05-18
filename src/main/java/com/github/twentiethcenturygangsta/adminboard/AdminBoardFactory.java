c8f7b598e5d0c5f076d480b02ecca2bafa470bcb
oereo <dlstpgns0406@gmail.com>
2023-05-18 13:24:07 +0900

    refactor: header와 sidebar를 fragment로 분리 및 controller에 등록

--- src/main/java/com/github/twentiethcenturygangsta/adminboard/AdminBoardFactory.java
+++ src/main/java/com/github/twentiethcenturygangsta/adminboard/AdminBoardFactory.java
@@ -3,12 +3,12 @@ package com.github.twentiethcenturygangsta.adminboard;
 import com.github.twentiethcenturygangsta.adminboard.client.EntityClient;
 import com.github.twentiethcenturygangsta.adminboard.entity.EntityInfo;
 import com.github.twentiethcenturygangsta.adminboard.repository.RepositoryClient;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
+@Slf4j
 @Service
 public class AdminBoardFactory {
     private final RepositoryClient repositoryClient;
@@ -19,18 +19,22 @@ public class AdminBoardFactory {
         this.entityClient = entityClient;
     }
 
-    public  HashMap<String, List<EntityInfo>> getEntities() {
-        HashMap<String, List<EntityInfo>> entitiesByGroup = new HashMap<>();
+    public  HashMap<String, ArrayList<EntityInfo>> getEntitiesByGroup() {
+        HashMap<String, ArrayList<EntityInfo>> entitiesByGroup = new HashMap<>();
 
         for(EntityInfo entityInfo: entityClient.getEntities()) {
             if(entitiesByGroup.containsKey(entityInfo.getGroup())) {
-                List<EntityInfo> entityInfos = entitiesByGroup.get(entityInfo.getGroup());
+                ArrayList<EntityInfo> entityInfos = entitiesByGroup.get(entityInfo.getGroup());
                 entityInfos.add(entityInfo);
                 entitiesByGroup.put(entityInfo.getGroup(), entityInfos);
             } else {
-                entitiesByGroup.put(entityInfo.getGroup(), List.of(entityInfo));
+                entitiesByGroup.put(entityInfo.getGroup(), new ArrayList<>(List.of(entityInfo)));
             }
         }
         return entitiesByGroup;
+    }
+
+    public List<EntityInfo> getEntities() {
+        return entityClient.getEntities();
     }
 }
--- src/main/java/com/github/twentiethcenturygangsta/adminboard/view/AdminBoardViewController.java
+++ src/main/java/com/github/twentiethcenturygangsta/adminboard/view/AdminBoardViewController.java
@@ -1,13 +1,15 @@
 package com.github.twentiethcenturygangsta.adminboard.view;
 
 import com.github.twentiethcenturygangsta.adminboard.AdminBoardFactory;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+@Slf4j
 @Controller
-@RequestMapping("admin-board")
+@RequestMapping("/admin-board")
 public class AdminBoardViewController {
     private final AdminBoardFactory adminBoardFactory;
 
@@ -17,9 +19,56 @@ public class AdminBoardViewController {
 
     @GetMapping("/home")
     public String HomeView(Model model) {
-        model.addAttribute("data", adminBoardFactory.getEntities());
+        model.addAttribute("data", adminBoardFactory.getEntitiesByGroup());
+        model.addAttribute("entities", adminBoardFactory.getEntities());
+        log.info("entities = {}", adminBoardFactory.getEntities());
+        log.info("data = {}", adminBoardFactory.getEntitiesByGroup());
+
         return "home";
     }
+
+    @GetMapping("/user")
+    public String ProfileView(Model model) {
+        model.addAttribute("data", adminBoardFactory.getEntities());
+        return "user";
+    }
+
+    @GetMapping("/table")
+    public String TableView(Model model) {
+        model.addAttribute("data", adminBoardFactory.getEntities());
+        return "table";
+    }
+
+    @GetMapping("/notifications")
+    public String NotificationView(Model model) {
+        model.addAttribute("data", adminBoardFactory.getEntities());
+        return "notifications";
+    }
+
+    @GetMapping("/icons")
+    public String IconsView(Model model) {
+        model.addAttribute("data", adminBoardFactory.getEntities());
+        return "icons";
+    }
+
+    @GetMapping("/maps")
+    public String MapsView(Model model) {
+        model.addAttribute("data", adminBoardFactory.getEntities());
+        return "maps";
+    }
+
+    @GetMapping("/typography")
+    public String TypographyView(Model model) {
+        model.addAttribute("data", adminBoardFactory.getEntities());
+        return "typography";
+    }
+
+    @GetMapping("/upgrade")
+    public String UpgradeView(Model model) {
+        model.addAttribute("data", adminBoardFactory.getEntities());
+        return "upgrade";
+    }
+
 
     @GetMapping("/login")
     public String LoginView(Model model) {
