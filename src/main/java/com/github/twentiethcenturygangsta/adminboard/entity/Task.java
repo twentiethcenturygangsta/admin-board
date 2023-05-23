51853efaf213470566100872692190fe05ab7b52
oereo <dlstpgns0406@gmail.com>
2023-05-23 12:23:51 +0900

    feat: Task Entity에 table에 대한 설명과 Column에 대한 설명 추가

--- src/main/java/com/github/twentiethcenturygangsta/adminboard/entity/Task.java
+++ src/main/java/com/github/twentiethcenturygangsta/adminboard/entity/Task.java
@@ -1,16 +1,22 @@
 package com.github.twentiethcenturygangsta.adminboard.entity;
 
+import com.github.twentiethcenturygangsta.adminboard.annotation.AdminBoardColumn;
+import com.github.twentiethcenturygangsta.adminboard.annotation.AdminBoardEntity;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import lombok.AccessLevel;
 import lombok.Builder;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.springframework.stereotype.Component;
 
 import java.time.LocalDateTime;
 
 @Entity
+@Getter
+@AdminBoardEntity(group = "AdminBoard", description = "대시보드 사용자의 Task 등록 노트 테이블입니다.")
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Task {
 
