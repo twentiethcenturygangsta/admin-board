5027fd4338b7404c07c6be72c342da6cd730c067
oereo <dlstpgns0406@gmail.com>
2023-05-25 13:25:47 +0900

    feat: AdminBoardInterceptor class 추가
    - session login이 안된 유저들에 대한 session 확인 로직 추가

---
+++ src/main/java/com/github/twentiethcenturygangsta/adminboard/AdminBoardInterceptor.java
@@ -0,0 +1,19 @@
+package com.github.twentiethcenturygangsta.adminboard;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+public class AdminBoardInterceptor implements HandlerInterceptor {
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        HttpSession session = request.getSession(false);
+        if (session == null || session.getAttribute(SessionConst.LOGIN_MEMBER) == null) {
+            response.sendRedirect("/admin-board/login");
+            return false;
+        }
+        return true;
+    }
+}
