8626871d78eb34af1407ff0670684b26e18a4d99
oereo <dlstpgns0406@gmail.com>
2023-06-06 14:01:42 +0900

    feat: getSearchObjects method 추가

--- src/main/java/com/github/twentiethcenturygangsta/adminboard/AdminBoardFactory.java
+++ src/main/java/com/github/twentiethcenturygangsta/adminboard/AdminBoardFactory.java
@@ -8,12 +8,9 @@ import com.github.twentiethcenturygangsta.adminboard.repository.RepositoryBuilde
 import com.github.twentiethcenturygangsta.adminboard.repository.RepositoryClient;
 import com.github.twentiethcenturygangsta.adminboard.repository.RepositoryInfo;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.domain.*;
 import org.springframework.data.repository.CrudRepository;
-import org.springframework.data.repository.ListCrudRepository;
-import org.springframework.data.repository.ListPagingAndSortingRepository;
+
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Service;
 
@@ -35,11 +32,11 @@ public class AdminBoardFactory {
         this.adminBoardClient = adminBoardClient;
     }
 
-    public  HashMap<String, ArrayList<EntityInfo>> getEntitiesByGroup() {
+    public HashMap<String, ArrayList<EntityInfo>> getEntitiesByGroup() {
         HashMap<String, ArrayList<EntityInfo>> entitiesByGroup = new HashMap<>();
 
-        for(EntityInfo entityInfo: entityClient.getEntities()) {
-            if(entitiesByGroup.containsKey(entityInfo.getGroup())) {
+        for (EntityInfo entityInfo : entityClient.getEntities()) {
+            if (entitiesByGroup.containsKey(entityInfo.getGroup())) {
                 ArrayList<EntityInfo> entityInfos = entitiesByGroup.get(entityInfo.getGroup());
                 entityInfos.add(entityInfo);
                 entitiesByGroup.put(entityInfo.getGroup(), entityInfos);
@@ -53,7 +50,8 @@ public class AdminBoardFactory {
     public List<EntityInfo> getEntities() {
         return entityClient.getEntities();
     }
-    public EntityInfo getEntity(String entityName){
+
+    public EntityInfo getEntity(String entityName) {
         return entityClient.getEntity(entityName);
     }
 
@@ -68,6 +66,10 @@ public class AdminBoardFactory {
         return adminBoardInfoTable;
     }
 
+//    public Object createObject(String entityName) {
+//
+//    }
+
     public Page<?> getObjects(String entityName, Pageable pageable) {
         RepositoryInfo repositoryInfo = repositoryClient.getRepository(entityName);
         Object repositoryObject = repositoryInfo.getRepositoryObject();
@@ -76,6 +78,46 @@ public class AdminBoardFactory {
 
         PagingAndSortingRepository<?, ?> repository = repositoryBuilder.build(PagingAndSortingRepository.class);
         return repository.findAll(pageable);
+    }
+
+    public <T> Page<T> getSearchObjects(String entityName, String searchType, String keyword, int pageIndex, int pageSize) {
+        RepositoryInfo repositoryInfo = repositoryClient.getRepository(entityName);
+        Object repositoryObject = repositoryInfo.getRepositoryObject();
+        PagingAndSortingRepository<?, ?> repository = (PagingAndSortingRepository<?, ?>) repositoryObject;
+        Sort sort = Sort.by(Sort.Direction.ASC, "id"); // 정렬 기준 필드와 방향 설정
+        Iterable<?> allEntities = repository.findAll(sort);
+        List<T> filteredEntities = new ArrayList<>();
+        for (Object entity : allEntities) {
+            // 검색 조건을 수동으로 확인하여 필터링
+            if (containsIgnoreCase(getEntityValue(entity, searchType), keyword)) {
+                filteredEntities.add((T) entity);
+            }
+        }
+
+        int start = pageIndex * pageSize;
+        int end = Math.min(start + pageSize, filteredEntities.size());
+        List<T> pageEntities = filteredEntities.subList(start, end);
+        Pageable pageable = PageRequest.of(pageIndex, pageSize);
+        return new PageImpl<>(pageEntities, pageable, filteredEntities.size());
+    }
+
+    private String getEntityValue(Object entity, String fieldName) {
+        try {
+            Field field = entity.getClass().getDeclaredField(fieldName);
+            field.setAccessible(true);
+            Object value = field.get(entity);
+            return String.valueOf(value);
+        } catch (NoSuchFieldException | IllegalAccessException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    private boolean containsIgnoreCase(String text, String keyword) {
+        if (text == null || keyword == null) {
+            return false;
+        }
+        return text.toLowerCase().contains(keyword.toLowerCase());
     }
 
     public Optional<Object> getObject(String entityName, Long entityObjectId) {
@@ -88,14 +130,10 @@ public class AdminBoardFactory {
         return repository.findById(entityObjectId);
     }
 
-    public Object getFieldMappingValue(Object root, String fieldName ) {
+    public Object getFieldMappingValue(Object root, String fieldName) {
         try {
-            Field field = root.getClass().getDeclaredField( fieldName );
-            Method getter = root.getClass().getDeclaredMethod(
-                    (field.getType().equals( boolean.class ) ? "is" : "get")
-                            + field.getName().substring(0, 1).toUpperCase( Locale.ROOT)
-                            + field.getName().substring(1)
-            );
+            Field field = root.getClass().getDeclaredField(fieldName);
+            Method getter = root.getClass().getDeclaredMethod((field.getType().equals(boolean.class) ? "is" : "get") + field.getName().substring(0, 1).toUpperCase(Locale.ROOT) + field.getName().substring(1));
 
             return getter.invoke(root);
         } catch (Exception e) {
