fc08f7528b6a749c0942facd692d3296e40edd60
oereo <dlstpgns0406@gmail.com>
2023-06-07 12:44:31 +0900

    feat: 특정 키워드를 포함한 객체 리스트를 반환하는 api와 객체 생성 api 추가

--- src/main/java/com/github/twentiethcenturygangsta/adminboard/view/AdminBoardViewActionController.java
+++ src/main/java/com/github/twentiethcenturygangsta/adminboard/view/AdminBoardViewActionController.java
@@ -1,5 +1,6 @@
 package com.github.twentiethcenturygangsta.adminboard.view;
 
+import com.github.twentiethcenturygangsta.adminboard.AdminBoardFactory;
 import com.github.twentiethcenturygangsta.adminboard.AdminBoardLoginService;
 import com.github.twentiethcenturygangsta.adminboard.AdminBoardServiceFactory;
 import com.github.twentiethcenturygangsta.adminboard.SessionConst;
@@ -7,24 +8,25 @@ import com.github.twentiethcenturygangsta.adminboard.user.AdminBoardUser;
 import com.github.twentiethcenturygangsta.adminboard.user.LoginRequestDto;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpSession;
-import org.springframework.stereotype.Controller;
+import org.springframework.http.ResponseEntity;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.HashMap;
 import java.util.Optional;
 
-@Controller
+@RestController
 @RequestMapping("/admin-board/action")
 public class AdminBoardViewActionController {
 
     private final AdminBoardServiceFactory adminBoardServiceFactory;
     private final AdminBoardLoginService adminBoardLoginService;
+    private final AdminBoardFactory adminBoardFactory;
 
-    public AdminBoardViewActionController(AdminBoardServiceFactory adminBoardServiceFactory, AdminBoardLoginService adminBoardLoginService) {
+    public AdminBoardViewActionController(AdminBoardServiceFactory adminBoardServiceFactory, AdminBoardLoginService adminBoardLoginService, AdminBoardFactory adminBoardFactory) {
         this.adminBoardServiceFactory = adminBoardServiceFactory;
         this.adminBoardLoginService = adminBoardLoginService;
+        this.adminBoardFactory = adminBoardFactory;
     }
 
     @PostMapping("/login")
@@ -38,6 +40,23 @@ public class AdminBoardViewActionController {
             model.addAttribute("error", "일치하는 대시보드 계정이 존재하지 않습니다.");
             return "login";
         }
+    }
 
+    @GetMapping("/{entityName}/search")
+    public ResponseEntity<Object> searchObjects(
+            @PathVariable("entityName") String entityName,
+            @RequestParam("keyword") String keyword,
+            @RequestParam("searchType") String searchType,
+            @RequestParam("pageIndex") int pageIndex,
+            @RequestParam("pageSize") int pageSize) {
+        return ResponseEntity.ok(adminBoardFactory.getSearchObjects(entityName, searchType, keyword, pageIndex, pageSize));
+    }
+
+    @PostMapping("/{entityName}/object")
+    public ResponseEntity<Object> createObject(
+            @PathVariable("entityName") String entityName,
+            @RequestBody HashMap<String, Object> object
+    ) {
+        return ResponseEntity.ok(adminBoardFactory.createObject(entityName, object));
     }
 }
