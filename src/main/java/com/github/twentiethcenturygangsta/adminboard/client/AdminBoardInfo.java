e9aaba9a56a2ce218a84bfb5ef3c9d01b970a6b6
oereo <dlstpgns0406@gmail.com>
2023-06-24 16:44:36 +0900

    feat: AdminBoardInfo class에 contactUrl field 추가

--- src/main/java/com/github/twentiethcenturygangsta/adminboard/client/AdminBoardInfo.java
+++ src/main/java/com/github/twentiethcenturygangsta/adminboard/client/AdminBoardInfo.java
@@ -8,23 +8,39 @@ public class AdminBoardInfo {
     private final String license;
     private final String licenseUrl;
     private final String version;
+    private final String contactUrl;
 
     @Builder
-    public AdminBoardInfo(String title, String description, String license, String licenseUrl, String version) {
+    public AdminBoardInfo(String title, String description, String license, String licenseUrl, String version, String contactUrl) {
         this.title = title;
         this.description = description;
         this.license = license;
         this.licenseUrl = licenseUrl;
         this.version = version;
+        this.contactUrl = contactUrl;
     }
 
     public String getTitle() {
         return title;
     }
+
     public String getDescription() {
         return description;
     }
-    public String getLicense() {return license;}
-    public String getLicenseUrl() {return licenseUrl;}
-    public String getVersion() {return version;}
+
+    public String getLicense() {
+        return license;
+    }
+
+    public String getLicenseUrl() {
+        return licenseUrl;
+    }
+
+    public String getVersion() {
+        return version;
+    }
+
+    public String getContactUrl() {
+        return contactUrl;
+    }
 }
